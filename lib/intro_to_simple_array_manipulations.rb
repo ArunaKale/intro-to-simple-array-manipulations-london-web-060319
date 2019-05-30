b26f4743e52ee3f77f3ebc474306e5be5657c85e
AN K <a2nkale@gmail.com>
2019-05-30 22:24:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,26 +21,3 @@ def shift_with_args(array)
   array.shift(2)
 end 
 
-def using_concat(array1, array2)
-  
-end 
-
-def using_insert(array,elelment)
-
-end 
-
-def using_uniq(array)
-
-end 
-
-def using_flatten(array())
-
-end 
-
-def using_delete(array,string)
-
-end 
-
-def using_delete_at(array,integer)
-
-end 
